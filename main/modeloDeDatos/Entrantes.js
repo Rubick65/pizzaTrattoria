b0f567d8dc676a7b60abda55d1b85ab909d7b597
Hugo de Pablo <hugdpalo@gmail.com>
2026-01-17 13:04:41 +0100

    Se me olvido subirlas ...

---
+++ main/modeloDeDatos/Entrantes.js
@@ -0,0 +1,29 @@
+class Entrantes extends Comida{
+
+    constructor(nombre,imagen,tamanio,precio,cantidad,descripcion) {
+        super(nombre,imagen,tamanio,precio,cantidad);
+        this.descripcion = descripcion;
+    }
+
+    constructor(nombre,imagen,tamanio,precio,cantidad,descripcion,unidades) {
+        super(nombre,imagen,tamanio,precio,cantidad);
+        this._descripcion = descripcion;
+        this._unidades = unidades;
+    }
+
+    get descripcion() {
+        return this._descripcion;
+    }
+
+    set descripcion(value) {
+        this._descripcion = value;
+    }
+
+    get unidades() {
+        return this._unidades;
+    }
+
+    set unidades(value) {
+        this._unidades = value;
+    }
+}
---
+++ main/modeloDeDatos/Postres.js
@@ -0,0 +1,24 @@
+class Postres extends Comida{
+
+    /**
+     * Modelo de datos del objeto postre
+     * @param nombre
+     * @param imagen
+     * @param tamanio
+     * @param precio
+     * @param cantidad
+     * @param calorias cantidad de calorias del postre en
+     */
+    constructor(nombre,imagen,tamanio,precio,cantidad,calorias) {
+        super(nombre,imagen,tamanio,precio,cantidad);
+        this._calorias = calorias;
+    }
+
+    get calorias() {
+        return this._calorias;
+    }
+
+    set calorias(value) {
+        this._calorias = value;
+    }
+}
