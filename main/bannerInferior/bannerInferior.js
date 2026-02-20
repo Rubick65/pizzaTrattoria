e03c4f5c2b1b6edf39d1bf30dcc2cf1ce7ebf070
David <minibatman9104@gmail.com>
2026-02-20 10:27:06 +0100

    comentado ofertas y banner supp e inf

--- main/bannerInferior/bannerInferior.js
+++ main/bannerInferior/bannerInferior.js
@@ -2,11 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const bannerInferiorContainer = document.getElementById("banner-inferior-container");
 
-    // Si la pagina no tiene banner inferior, no hacemos na
+    // Si la pagina no tiene banner inferior, no hacemos nada
     if (!bannerInferiorContainer) return;
 
+    // Creamos el banner dinámicamente
     crearBannerInferior();
 
+    // Insertamos la estructura HTML del banner dentro del contenedor
     function crearBannerInferior() {
 
         bannerInferiorContainer.innerHTML = `
--- main/ofertas/ofertas.js
+++ main/ofertas/ofertas.js
@@ -1,11 +1,19 @@
+// Espera a que cargue el DOM
 document.addEventListener("DOMContentLoaded", () => {
 
+    // Selecciona el contenedor que se mueve
     const track = document.querySelector(".carousel-track");
     let index = 0;
 
+    // Cambia de imagen automáticamente cada 6 segundos
     setInterval(() => {
+
+        // Alterna entre 0 y 1 (las dos imágenes)
         index = (index + 1) % 2;
+
+        // Desplaza el carrusel horizontalmente
         track.style.transform = `translateX(-${index * 50}%)`;
-    }, 6000); // 6 segundos
+
+    }, 6000);
 
 });
